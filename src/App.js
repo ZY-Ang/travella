6acabd1c123ab7803de043cf6367d6b19aa436c7
Alex Ang <E0032221@u.nus.edu>
2018-05-19 17:47:53 -0400

    Add HomePage component and reroute dashboard

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import './scss/style.css'
 // Containers
 import { DefaultLayout } from './containers';
 // Pages
-import { Page404, Page500 } from './views/Pages';
+import { PageHome, Page404, Page500 } from './views/Pages';
 
 // import { renderRoutes } from 'react-router-config';
 
@@ -27,7 +27,8 @@ class App extends Component {
         <Switch>
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />
-          <Route path="/" name="Home" component={DefaultLayout} />
+          <Route path="/" name="Home" component={PageHome} />
+          <Route path="/Dashboard" name="Dashboard" component={DefaultLayout} />
         </Switch>
       </HashRouter>
     );
--- src/views/Pages/index.js
+++ src/views/Pages/index.js
@@ -1,6 +1,8 @@
 import Page404 from './Page404';
 import Page500 from './Page500';
+import PageHome from "./Home";
 
 export {
+  PageHome,
   Page404, Page500
 };
